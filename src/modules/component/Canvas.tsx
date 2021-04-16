47d39d238e0079dbca3f01fbed02ef8930499682
Emilio Torres Jr <emiliotorres3193@gmail.com>
2021-04-16 01:17:01 -0500

    Added BoundingBox Rendering

--- src/modules/component/Canvas.tsx
+++ src/modules/component/Canvas.tsx
@@ -1,11 +1,17 @@
-import React, {useEffect, useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { fabric } from 'fabric';
 import { FabricJSCanvas, useFabricJSEditor } from 'fabricjs-react';
-import CanvasButtons from "./CanvasButtons";
+import { Annotation } from "../../types/dataset/Annotation";
 import { Image } from "../../types/dataset/Image";
+import CanvasButtons from "./CanvasButtons";
+import {Object} from "fabric/fabric-impl";
+
+const DEFAULT_WIDTH = 50;
+const DEFAULT_HEIGHT = 50;
 
 type CanvasProps = {
     images: Image[];
+    annotations: Annotation[];
 };
 
 const Canvas = (props: CanvasProps) => {
@@ -18,44 +24,49 @@ const Canvas = (props: CanvasProps) => {
     };
 
     const addRectangle = () => {
-        const rectangle = new fabric.Rect({
-            originX: "left",
-            originY: "top",
-            fill: "transparent",
-            stroke: "#992C7E",
-            left: (editor?.canvas.width || 0) / 2 - 25,
-            top: (editor?.canvas.height || 0) / 2 - 25,
-            width: 50,
-            height: 50,
-            hasBorders: false,
-            lockRotation: true,
-            strokeUniform: true,
-        });
-        rectangle.setControlVisible("mtr", false);
-        editor?.canvas.add(rectangle);
+        if (editor) {
+            const rectangle = new fabric.Rect({
+                originX: "left",
+                originY: "top",
+                fill: "transparent",
+                stroke: "#992C7E",
+                left: ((editor.canvas.width || 0) - DEFAULT_WIDTH) / 2,
+                top: ((editor.canvas.height || 0) - DEFAULT_HEIGHT) / 2,
+                width: DEFAULT_WIDTH,
+                height: DEFAULT_HEIGHT,
+                hasBorders: false,
+                lockRotation: true,
+                strokeUniform: true,
+            });
+            rectangle.setControlVisible("mtr", false);
+            editor.canvas.add(rectangle);
+        }
     };
 
     const addCircle = () => {
-        const circle = new fabric.Circle({
-            originX: "center",
-            originY: "center",
-            fill: "transparent",
-            stroke: "#992C7E",
-            left: (editor?.canvas.width || 0) / 2,
-            top: (editor?.canvas.height || 0) / 2,
-            radius: 25,
-            hasBorders: false,
-            lockRotation: true,
-            strokeUniform: true,
-        });
-        circle.setControlVisible("mtr", false);
-        editor?.canvas.add(circle);
+        if (editor) {
+            const circle = new fabric.Circle({
+                originX: "center",
+                originY: "center",
+                fill: "transparent",
+                stroke: "#992C7E",
+                left: (editor.canvas.width || 0) / 2,
+                top: (editor.canvas.height || 0) / 2,
+                radius: 25,
+                hasBorders: false,
+                lockRotation: true,
+                strokeUniform: true,
+            });
+            circle.setControlVisible("mtr", false);
+            editor.canvas.add(circle);
+        }
     };
 
     const zoomIn = () => editor?.zoomIn();
     const zoomOut = () => editor?.zoomOut();
-    const previousImage = () => setImageIndex((idx: number) => Math.max(0, Math.min(idx - 1, props.images.length - 1)));
-    const nextImage = () => setImageIndex((idx: number) => Math.max(0, Math.min(idx + 1, props.images.length - 1)));
+    const normalizeIndex = (idx: number) => Math.max(0, Math.min(idx, props.images.length - 1));
+    const previousImage = () => setImageIndex((idx: number) => normalizeIndex(idx - 1));
+    const nextImage = () => setImageIndex((idx: number) => normalizeIndex(idx + 1));
 
     const deleteObjects = () => {
         if ((selectedObjects?.length || 0) > 0) {
@@ -65,14 +76,44 @@ const Canvas = (props: CanvasProps) => {
         }
     };
 
-    useEffect(() => {
-        if (editor?.canvas && props.images.length > 0) {
-            const idx: number = Math.max(0, Math.min(imageIndex, props.images.length - 1));
-            editor?.canvas.setWidth(props.images[idx].width);
-            editor?.canvas.setHeight(props.images[idx].height);
-            editor?.canvas.setBackgroundImage(props.images[idx].coco_url, editor?.canvas.renderAll.bind(editor?.canvas));
+    const loadImage = () => {
+        if (editor?.canvas && props.images.length > imageIndex) {
+            const image: Image = props.images[imageIndex];
+            editor.canvas.setWidth(image.width);
+            editor.canvas.setHeight(image.height);
+            editor.canvas.setBackgroundImage(image.coco_url, editor.canvas.renderAll.bind(editor.canvas));
         }
-    }, [editor?.canvas, imageIndex, props.images]);
+    };
+
+    const loadAnnotations = () => {
+        if (editor?.canvas && props.images.length > imageIndex) {
+            editor.canvas.getObjects().forEach((obj: Object) => editor.canvas.remove(obj));
+            const image: Image = props.images[imageIndex];
+            props.annotations.filter((annotation: Annotation) => annotation.image_id === image.id)
+                .filter((annotation: Annotation) => annotation.bbox !== undefined)
+                .filter((annotation: Annotation) => annotation.bbox.length === 4)
+                .forEach((annotation: Annotation) => {
+                    const rectangle = new fabric.Rect({
+                        originX: "left",
+                        originY: "top",
+                        fill: "transparent",
+                        stroke: "#992C7E",
+                        left: annotation.bbox[0],
+                        top: annotation.bbox[1],
+                        width: annotation.bbox[2],
+                        height: annotation.bbox[3],
+                        hasBorders: false,
+                        lockRotation: true,
+                        strokeUniform: true,
+                    });
+                    rectangle.setControlVisible("mtr", false);
+                    editor?.canvas.add(rectangle);
+                });
+        }
+    };
+
+    useEffect(loadImage, [editor?.canvas, props.images, imageIndex]);
+    useEffect(loadAnnotations, [editor?.canvas, props.images, imageIndex, props.annotations]);
 
     return (
         <div>
--- src/modules/node/AnnotatorNode.tsx
+++ src/modules/node/AnnotatorNode.tsx
@@ -4,18 +4,21 @@ import { State } from "../../redux/store";
 import { Dispatch } from "redux";
 import { connect } from "react-redux";
 import { Image } from "../../types/dataset/Image";
+import {Annotation} from "../../types/dataset/Annotation";
 
 type AnnotatorNodeProps = {
     images: Image[];
+    annotations: Annotation[];
 };
 
 const ConnectedAnnotatorNode = (props: AnnotatorNodeProps) => {
-    return <Canvas images={props.images} />;
+    return <Canvas images={props.images} annotations={props.annotations} />;
 };
 
 const mapStateToProps = (state: State) => {
     return {
-        images: state.dataset?.images || []
+        images: state.dataset?.images || [],
+        annotations: state.dataset?.annotations || []
     };
 };
 
